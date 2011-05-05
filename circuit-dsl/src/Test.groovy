b7d3210d965b1ee9c6830d37cd61a9231b6ebbca
Demian Ferreiro <epidemian@gmail.com>
2011-05-05 05:23:22 -0300

    Money and bank accounts DSL test.

--- circuit-dsl/src/Test.groovy
+++ circuit-dsl/src/Test.groovy
@@ -1,10 +1,93 @@
+import groovy.transform.Canonical
 
-/**
- * Created by IntelliJ IDEA.
- * User: demian
- * Date: 5/5/11
- * Time: 3:45 AM
- * To change this template use File | Settings | File Templates.
- */
+// Normal class definitions. Boring stuff.
 
-println "Hello World!" 
+@Canonical
+class Amount {
+    Number value
+    String unit
+
+    Amount plus(Amount other) {
+        checkUnit(other)
+        new Amount(value + other.value, unit)
+    }
+
+    Amount minus(Amount other) {
+        checkUnit(other)
+        new Amount(value - other.value, unit)
+    }
+
+    Amount negative() {
+        new Amount(-value, unit)
+    }
+
+    void checkUnit(Amount other) {
+        if (unit != other.unit)
+            throw new Exception("Invalid unit")
+    }
+
+    String toString() { "$value $unit" }
+}
+
+@Canonical
+class Account {
+    String name
+    Amount balance
+
+    def deposit(amount) {
+        balance += amount
+    }
+
+    def withdraw(amount) {
+        balance -= amount
+    }
+
+    def transfer(Account to, Amount amount) {
+        this.withdraw(amount)
+        to.deposit(amount)
+    }
+}
+
+// Amounts DSL definition.
+Number.metaClass.getProperty = { String unit -> new Amount(delegate, unit) }
+
+// Amounts DSL usage.
+assert 3.pesos + 2.pesos == 5.pesos
+assert 2.peras != 2.manzanas
+assert 5.kiwis - 7.kiwis == -2.kiwis // negative() called.
+
+// Accounts DSL definition.
+def deposit = { Amount amount ->
+    [into: { Account account ->
+        account.deposit amount
+    }]
+}
+
+def withdraw = { Amount amount ->
+    [from: { Account account ->
+        account.withdraw amount
+    }]
+}
+
+def transfer = { Amount amount ->
+    [from: { Account from ->
+        [to: {Account to ->
+            from.transfer to, amount
+        }]
+    }]
+}
+
+// Accounts DSL usage:
+def estebanAccount = new Account("Esteban", 500.pesos)
+
+deposit 100.pesos into estebanAccount
+assert estebanAccount.balance == 600.pesos
+
+withdraw 1000.pesos from estebanAccount
+assert estebanAccount.balance == -400.pesos
+
+def joaquinAccount = new Account("Joaquin", 4000.pesos)
+
+transfer 544.pesos from joaquinAccount to estebanAccount
+assert estebanAccount.balance == 144.pesos
+assert joaquinAccount.balance == 3456.pesos
