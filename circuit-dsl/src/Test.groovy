6b263579e6d6ab9548f8a25be0016434f9b881fb
Demian Ferreiro <epidemian@gmail.com>
2011-05-05 03:48:02 -0300

    First commit of Circuit DSL project.

---
+++ circuit-dsl/src/Test.groovy
@@ -0,0 +1,10 @@
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: demian
+ * Date: 5/5/11
+ * Time: 3:45 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+println "Hello World!" 
