72bad1835b0aa4d086aef4d53a10386c6fb99e21
Demian Ferreiro <epidemian@gmail.com>
2011-09-30 06:46:01 -0300

    Python 3 unicode support FTW

--- stuff/widen.py
+++ stuff/widen.py
@@ -1,33 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # ^ That makes it possible to write unicode stuff ＬＩＫＥ　ＴＨＩＳ in code :)
 
 def widen_chr(c):
     if 'A' <= c.upper() <= 'Z':
-        return unichr(ord(u'Ａ') + ord(c) - ord('A'))
+        return chr(ord('Ａ') + ord(c) - ord('A'))
     elif c == ' ':
-        return u'　'
+        return '　'
     else:
         return c
 
 def widen(s):
     return ''.join(widen_chr(c) for c in s)
     
+from optparse import OptionParser
 
-import argparse
+desc = "Widen some text. For example '%prog Hello World' will print "\
+    "'Ｈｅｌｌｏ　Ｗｏｒｌｄ'"
+parser = OptionParser(description=desc)
+options, args = parser.parse_args()
 
-epilog = u'''
-Usage example:
-$ %(prog)s Hello World
-Ｈｅｌｌｏ　Ｗｏｒｌｄ
-'''.encode('utf-8') 
-# ^ encode call needed as argparse does not support unicode strings.
-
-parser = argparse.ArgumentParser(description='Widen some text.', epilog=epilog,
-    formatter_class=argparse.RawDescriptionHelpFormatter)
-parser.add_argument('strings', metavar='TEXT', type=str, 
-    nargs='+', help='text to widen')
-
-args = parser.parse_args()
-wide_str = u'　'.join(widen(s) for s in args.strings)
-print(wide_str)
+wide_args = (widen(arg) for arg in args)
+print(*wide_args, sep='　')
